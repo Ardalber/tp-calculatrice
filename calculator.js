2c39cb2a0b8140905efe3b5829aa782948ae09d6
Ardalber <[email-du-dveloppeur]cochezgaetan07@gmail.com>
2020-12-08 16:53:36 +0100

    ajout du fichier calculator.js

---
+++ calculator.js
@@ -0,0 +1,60 @@
+'use strict';
+
+
+const button1 = document.createElement('div');
+const button2 = document.createElement('div');
+const button3 = document.createElement('div');
+const button4 = document.createElement('div');
+const button5 = document.createElement('div');
+const button6 = document.createElement('div');
+const button7 = document.createElement('div');
+const button8 = document.createElement('div');
+const button9 = document.createElement('div');
+const button0 = document.createElement('div');
+const buttonPlus = document.createElement('div');
+const buttonMoins = document.createElement('div');
+const buttonMult = document.createElement('div');
+const buttonDivide = document.createElement('div');
+const buttonEqual = document.createElement('div');
+const buttonAc = document.createElement('div');
+const buttonDot = document.createElement('div');
+
+
+const reference = document.body;
+
+reference.append(button1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
